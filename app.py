cebaf4cf19d095c7e0a1e517c5033f34d44c3577
Vincent Yang <48404862+Vincent550102@users.noreply.github.com>
2021-10-19 18:04:39 +0800

    host upd & db Persistence

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def forbidden(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8080)
+    app.run(host='0.0.0.0', port=80)
--- schema.sql
+++ schema.sql
@@ -1,17 +1,17 @@
 -- Initialize the database.
 -- Drop any existing data and create empty tables.
 
-DROP TABLE IF EXISTS `url_map`;
-DROP TABLE IF EXISTS `unuse_codes`;
+-- DROP TABLE IF EXISTS `url_map`;
+-- DROP TABLE IF EXISTS `unuse_codes`;
 
-CREATE TABLE url_map (
+CREATE TABLE IF NOT EXISTS url_map (
   id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   code VARCHAR(10) UNIQUE NOT NULL,
   `url` VARCHAR(750) NOT NULL,
   author VARCHAR(30) NOT NULL,
   clicked INT(20) NOT NULL
 );
-CREATE TABLE unuse_codes (
+CREATE TABLE IF NOT EXISTS unuse_codes (
   id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   code VARCHAR(10) NOT NULL
 );
